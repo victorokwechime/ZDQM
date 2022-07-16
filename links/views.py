8d9753f4aa98c95f4b849c9ff88f8ea7cd33e2db
victorokwechime <victorokwechime1@gmail.com>
2022-07-16 23:51:49 +0100

    DQ&M

---
+++ links/views.py
@@ -0,0 +1,58 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from . import models 
+from . import serializers
+
+import datetime 
+
+from rest_framework.generics import ListAPIView, DestroyAPIView
+from rest_framework.generics import CreateAPIView, RetrieveAPIView, UpdateAPIView
+from .serializers import LinkSerializer
+from .models import Link
+# Create your views here.
+class PostListApi(ListAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+    
+class PostCreateApi(CreateAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+class PostDetailApi(RetrieveAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+class PostUpdateApi(UpdateAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+class PostDeleteApi(DestroyAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+class ActiveLinkView(APIView):
+    """
+    Returns a list of all active (publicly accessible) links
+    """
+    def get(self, request):
+        """ 
+        Invoked whenever a HTTP GET Request is made to this view
+        """
+        qs = models.Link.public.all()
+        data = serializers.LinkSerializer(qs, many=True).data
+        return Response(data, status=status.HTTP_200_OK)
+    
+class RecentLinkView(APIView):
+    """
+    Returns a list of recently created active links
+    """
+    def get(self, request):
+        """ 
+        Invoked whenever a HTTP GET Request is made to this view
+        """
+        seven_days_ago = timezone.now() - datetime.timedelta(days=7)
+        qs = models.Link.public.filter(created_date__gte=seven_days_ago)
+        data = serializers.LinkSerializer(qs, many=True).data
+        return Response(data, status=status.HTTP_200_OK)
